27311901bcd5e56b8ef3540363c2dc1dcaeb148c
Szilvássy Bence <benceszilvassy91@gmail.com>
2023-07-21 09:00:45 +0200

    uezenet

---
+++ practicejs.js
@@ -0,0 +1,20 @@
+let teams = ["Arsenal", "Liverpool", "Manchester City", "Manchester United", "Chelsea", "Tottenham"];
+
+
+let players = ["Martinelli", "Salah", "Haaland", "Rashford", "Kane"];
+
+let random = Math.floor(Math.random()*6);
+
+//console.log(team[randomTeam]);
+
+//console.log(teams);
+
+//console.log(players);
+
+let randomTeam = teams[random];
+
+let randomPlayer = players[random];
+
+console.log(teams[random]) + " " + console.log(players[random]);
+
+
